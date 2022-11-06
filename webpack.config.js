b753f01d21886f03f8f0d723004d2c4577d1076f
Tore Bjolseth <tbjolset@cisco.com>
2022-11-06 20:52:01 +0100

    add webpack dev server for easier dev life

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './client/js/index.js',
+  devServer: {
+    static: './client',
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'client'),
